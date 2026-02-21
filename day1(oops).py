222b40858a0e13eda3be5e0eb0c45ef5660c1e53
Himanshu Shekhar <148846385+himanshu-shekhar01@users.noreply.github.com>
2026-02-21 22:37:36 +0530

    Add files via upload

---
+++ day1(oops).py
@@ -0,0 +1,13 @@
+# by function redundancy decrease and reuseability increase
+
+# class student():
+#     name = "himasnhu"
+#     roll = "engineer"
+    
+# obj1 =student()
+# print(obj1)
+# print(obj1.roll)
+
+# obj2 = student()
+
+ 
